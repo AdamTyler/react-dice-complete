d6e5d22983a6336907666bde2487725af731ba3f
Adam Tyler <lazy9669@gmail.com>
2017-03-11 13:17:54 -0600

    Adding footer and license

--- src/TestApp.js
+++ src/TestApp.js
@@ -57,37 +57,37 @@ class TestApp extends Component {
     return (
       <div className="dice-test">
         <form className="row controls align-items-end">
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <label htmlFor="numDice">Dice</label>
             <input type="number" name="numDice" id="numDice" className="form-control"
               value={state.numDice} onChange={this.handleChange} min="1" max="100"/>
           </fieldset>
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <label htmlFor="faceColor">Face Color</label>
             <input type="color" name="faceColor" id="faceColor" className="form-control" style={colorStyle}
               value={state.faceColor} onChange={this.handleChange} />
           </fieldset>
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <label htmlFor="dotColor">Dot Color</label>
             <input type="color" name="dotColor" id="dotColor" className="form-control" style={colorStyle}
               value={state.dotColor} onChange={this.handleChange} />
           </fieldset>
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <label htmlFor="dieSize">Die Size (px)</label>
             <input type="number" name="dieSize" id="dieSize" className="form-control"
               value={state.dieSize} onChange={this.handleChange} min="30" max="200"/>
           </fieldset>
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <label htmlFor="dieSize">Margin Between (px)</label>
             <input type="number" name="margin" id="margin" className="form-control"
               value={state.margin} onChange={this.handleChange} min="0" max="200"/>
           </fieldset>
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <label htmlFor="rollTime">Roll Time (seconds)</label>
             <input type="number" name="rollTime" id="rollTime" className="form-control"
               value={state.rollTime} onChange={this.handleChange} min="1" max="4"/>
           </fieldset>
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <div className="form-check">
               <label className="form-check-label">
                 <input type="checkbox" className="form-check-input" name="outline" id="outline"
@@ -101,15 +101,7 @@ class TestApp extends Component {
                 disabled={!this.state.outline}/>
             </div>
           </fieldset>
-        </form>
-        <div className="row info">
-          <div className="col">
-            <h4>
-              <button className="btn btn-primary" onClick={this.rollAll}>Roll All</button>
-              {'   '} or click individual dice
-            </h4>
-          </div>
-          <fieldset className="form-group col">
+          <fieldset className="form-group col-xs-6 col-sm-4 col-md-3">
             <div className="form-check">
               <label className="form-check-label">
                 <input type="checkbox" className="form-check-input" name="disableIndividual" id="disableIndividual"
@@ -118,6 +110,14 @@ class TestApp extends Component {
               </label>
             </div>
           </fieldset>
+        </form>
+        <div className="row info">
+          <div className="col">
+            <h4>
+              <button className="btn btn-primary" onClick={this.rollAll}>Roll All</button>
+              {'   '} or click individual dice
+            </h4>
+          </div>
 
           <div className="col">
             <h4 className="text-primary">Dice Total:
